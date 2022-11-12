9ed68d89f356cd2702de245fa42bf827c00e7950
Karel Peeters <karel.peeters.leuven@gmail.com>
2022-11-12 10:52:42 +0100

    Drop rarely used tokens in favor of newly added ones.

--- kt-core/src/bin/pick_tokens.rs
+++ kt-core/src/bin/pick_tokens.rs
@@ -1,4 +1,5 @@
-use std::cmp::min;
+use std::cmp::{min, Reverse};
+use std::fmt::{Debug, Formatter};
 use std::fs::File;
 use std::io::{BufWriter, Write};
 use std::path::PathBuf;
@@ -6,7 +7,7 @@ use std::time::Instant;
 
 use clap::Parser;
 use itertools::Itertools;
-use ndarray::{s, Array2};
+use ndarray::{s, Array2, Zip};
 use serde::Serialize;
 
 use kt_core::batch::build_tokenizer;
@@ -18,8 +19,15 @@ struct Args {
     input: PathBuf,
     output: PathBuf,
 
+    debug_path: Option<PathBuf>,
+
     #[clap(long, default_value_t = 1024)]
-    tokens: usize,
+    max_tokens: usize,
+    #[clap(long, default_value_t = 1024)]
+    max_drops: usize,
+
+    #[clap(long, default_value_t = 2.0)]
+    threshold_drop_factor: f32,
 
     #[clap(long, default_value_t = 10000)]
     threshold_count: u32,
@@ -57,7 +65,9 @@ fn main() -> std::io::Result<()> {
     assert_eq!("json", args.output.extension().unwrap());
     std::fs::create_dir_all(args.output.parent().unwrap())?;
 
-    let max_tokens = args.tokens;
+    let max_tokens = args.max_tokens;
+    let max_drops = args.max_drops;
+    let threshold_drop_factor = args.threshold_drop_factor;
     let threshold_count = args.threshold_count;
     let threshold_samples = args.threshold_samples;
     assert!(threshold_count < Count::MAX);
@@ -71,15 +81,22 @@ fn main() -> std::io::Result<()> {
     let mut is_whitespace = (0..u8::MAX)
         .map(|c| (c as char).is_whitespace())
         .collect_vec();
+    let forced_token_count = u8::MAX as usize;
 
     let mut aho = build_tokenizer(&tokens);
 
     let mut bigram_count: Array2<Count> = Array2::zeros((max_tokens, max_tokens));
+    let mut unigram_count: Vec<Count> = vec![0; tokens.len()];
+    let mut has_been_merged: Vec<bool> = vec![false; tokens.len()];
+
     let mut tokens_since_add = 0;
     let mut samples_since_add = 0;
+    let mut drops_applied = 0;
     let mut top_count = 0;
     let mut top_index = None;
     let mut prev_time = Instant::now();
+
+    let mut dropped_tokens = vec![];
 
     let sample_iter = FlatRepeatResult::new(|| -> std::io::Result<_> {
         println!("Start decoding from start of file");
@@ -100,6 +117,7 @@ fn main() -> std::io::Result<()> {
             let curr_token = x.pattern();
 
             tokens_since_add += 1;
+            unigram_count[curr_token] += 1;
 
             if let Some(prev_token) = prev_token {
                 // only combine tokens that are both or neither whitespace
@@ -122,15 +140,16 @@ fn main() -> std::io::Result<()> {
                 samples_since_add, tokens_since_add, top_count
             );
 
+            let now = Instant::now();
+            let (top_a, top_b) = top_index.unwrap();
+
             // add top token
             {
-                let now = Instant::now();
-                let (top_a, top_b) = top_index.unwrap();
                 assert_eq!(is_whitespace[top_a], is_whitespace[top_b]);
 
                 let new_token = [tokens[top_a].as_slice(), tokens[top_b].as_slice()].concat();
                 println!(
-                    "Adding token {}: {:?} {:?} with count {} after {:?}",
+                    "  token {}: {:?} {:?} with count {} after {:?}",
                     tokens.len(),
                     String::from_utf8_lossy(&new_token),
                     new_token,
@@ -140,9 +159,73 @@ fn main() -> std::io::Result<()> {
 
                 tokens.push(new_token);
                 is_whitespace.push(is_whitespace[top_a]);
+                has_been_merged[top_a] |= true;
+                has_been_merged[top_b] |= true;
+                has_been_merged.push(false);
 
                 bigram_count[(top_a, top_b)] = 0;
                 prev_time = now;
+
+                // inherit bigram count
+                unigram_count.push(top_count);
+                unigram_count[top_a] -= top_count;
+                unigram_count[top_b] -= top_count;
+            }
+
+            // TODO immediately drop tokens with counts that reach zero
+            // TODO have some variance estimate for token counts, and only remove tokens below eg. 2*sigma
+            // TODO immediately add all tokens with counts > over threshold
+            //   overlapping ones will decay and be removed later anyway
+            // TODO decay per sample instead of per added token somehow?
+            //   maybe just decay on access (and store last modification time)
+
+            // find least used token that has been merged, skip forced & last token
+            let least_used_token = unigram_count[forced_token_count..unigram_count.len() - 1]
+                .iter()
+                .enumerate()
+                .map(|(i, &c)| (i + forced_token_count, c))
+                .filter(|&(i, _)| has_been_merged[i])
+                .min_by_key(|&(_, c)| c)
+                .map(|(i, _)| i);
+
+            if let Some(least_used_token) = least_used_token {
+                let least_used_count = unigram_count[least_used_token];
+
+                println!(
+                    "Least used token: {:?} with count {} ({} drops)",
+                    ByteString(&tokens[least_used_token]),
+                    least_used_count,
+                    drops_applied,
+                );
+
+                // possibly remove least-used token
+                if least_used_count == 0
+                    || (tokens.len() == max_tokens
+                        && drops_applied < max_drops
+                        && ((least_used_count as f32 * threshold_drop_factor) as Count) < top_count)
+                {
+                    // if it's the least used by a large enough margin and not the most recent token
+                    drops_applied += 1;
+                    println!(
+                        "Removing token {:?} with only {} uses",
+                        ByteString(&tokens[least_used_token]),
+                        least_used_count
+                    );
+
+                    let a = least_used_token;
+                    let b = tokens.len() - 1;
+
+                    // remove by swapping everything
+                    dropped_tokens.push((tokens.swap_remove(a), least_used_count));
+                    unigram_count.swap_remove(a);
+                    has_been_merged.swap_remove(a);
+
+                    // 2D array, one swap operation per axis
+                    let (a_slice, b_slice) = bigram_count.multi_slice_mut((s![a, ..], s![b, ..]));
+                    Zip::from(a_slice).and(b_slice).for_each(std::mem::swap);
+                    let (a_slice, b_slice) = bigram_count.multi_slice_mut((s![.., a], s![.., b]));
+                    Zip::from(a_slice).and(b_slice).for_each(std::mem::swap);
+                }
             }
 
             // invalidate state
@@ -163,6 +246,10 @@ fn main() -> std::io::Result<()> {
                     let scaled = clipped as u32 * count_decay_numerator / count_decay_denominator;
                     scaled as Count
                 });
+
+            unigram_count.iter_mut().for_each(|c| {
+                *c = (*c as u32 * count_decay_numerator / count_decay_denominator) as Count
+            });
         }
 
         if tokens.len() >= max_tokens {
@@ -170,13 +257,30 @@ fn main() -> std::io::Result<()> {
         }
     }
 
-    println!("Final tokens:");
-    for token in &tokens {
-        if let Ok(token) = std::str::from_utf8(token) {
-            println!("  {:?}", token);
-        } else {
-            println!("  {:?}", token);
+    if let Some(debug_path) = &args.debug_path {
+        let mut debug_writer = BufWriter::new(File::create(debug_path)?);
+
+        let mut indices = (0..tokens.len()).collect_vec();
+        indices.sort_by_key(|&i| Reverse(unigram_count[i]));
+
+        writeln!(&mut debug_writer, "Token: (token: count forced)")?;
+        for i in indices {
+            writeln!(
+                &mut debug_writer,
+                "  {:?}: {} {}",
+                ByteString(&tokens[i]),
+                unigram_count[i],
+                i < forced_token_count,
+            )?;
         }
+
+        writeln!(&mut debug_writer, "\n\nDropped tokens:")?;
+        for (token, count) in dropped_tokens {
+            writeln!(&mut debug_writer, "  {:?}: {}", ByteString(&token), count)?;
+        }
+
+        debug_writer.flush()?;
+        drop(debug_writer);
     }
 
     println!("Writing output file");
@@ -186,4 +290,16 @@ fn main() -> std::io::Result<()> {
     vocab_writer.flush()?;
 
     Ok(())
+}
+
+struct ByteString<'a>(&'a [u8]);
+
+impl<'a> Debug for ByteString<'a> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        if let Ok(s) = std::str::from_utf8(self.0) {
+            write!(f, "{:?}", s)
+        } else {
+            write!(f, "{:?}", self.0)
+        }
+    }
 }
